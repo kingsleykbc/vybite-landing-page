f0c6483a5ca761ceb9187f55c2e4d13fe80fac5b
Kingsley <kingsleybestconcept@gmail.com>
2018-12-17 08:52:56 +0100

    updated the time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 20, 2019 15:37:25").getTime();
+var countDownDate = new Date("Feb 14, 2019 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
